4865f8440a8b1260cc6b7c22d55811ca10a28ab5
Imam Yunianto <imamyunianto@gmail.com>
2026-09-24 14:40:54 +0700

    day 21

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,11 @@
+import java.util.Properties
+val localProperties = Properties().apply {
+    val f = rootProject.file("local.properties")
+    if (f.exists()) {
+        load(f.inputStream())
+    }
+}
+
 plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
@@ -27,6 +35,9 @@ android {
         // flag during build.
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        
+        manifestPlaceholders["GOOGLE_MAPS_API_KEY"] =
+        localProperties.getProperty("GOOGLE_MAPS_API_KEY", "")
     }
 
     buildTypes {
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'screens/home_screen.dart';
 
 void main() {
